7b5866b8fd4d19280aebdb526d29f7d0993aab76
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-13 19:04:36 -0500

    Write barebones server

---
+++ coupon-api.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const config = require('./models/config');
+
+var app = express();
+var router = express.Router();
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/', router);
+
+// handle 404
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        console.log(err);
+        res.status(err.status || 500).send();
+    });
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).send();
+});
+
+var server = app.listen(config.port);
+console.log('Listening at http://localhost:%s in %s mode',
+    server.address().port, app.get('env'));
+
+module.exports = app;
