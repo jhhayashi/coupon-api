3c3f00322270aea9c14548a1cb758784a450f59b
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-17 12:26:25 -0800

    Add all endpoints for coupons

--- controllers/coupons.js
+++ controllers/coupons.js
@@ -7,10 +7,20 @@ exports.getAllCoupons = (req, res, next) => {
     });
 };
 
-// TODO
 exports.getActiveCoupons = (req, res,next) => {
     var now = new Date();
-    res.json([]);
+    Coupon.find({
+        $and: [
+            { startDate: {$lt: now} },
+            { $or: [
+                { endDate: {$gt: now} },
+                { endDate: {$exists: false} },
+            ]}
+        ]
+    }, (err, coupons) => {
+        if (err) return next(err);
+        res.json(coupons);
+    });
 };
 
 exports.getCouponById = (req, res, next) => {
@@ -30,12 +40,19 @@ exports.createCoupon = (req, res, next) => {
     });
 };
 
-// TODO
+// TODO verification
 exports.updateCoupon = (req, res, next) => {
-    return res.sendStatus(200);
+    Coupon.findOneAndUpdate({id: req.params.id}, req.body, (err, coupon) => {
+        if (err) return next(err);
+        return res.sendStatus(200);
+    });
 };
 
-// TODO
+// TODO auth
 exports.deleteCouponById = (req, res, next) => {
-    return res.sendStatus(200);
+    Coupon.findByIdAndRemove(req.params.id, (err, coupon) => {
+        if (err) return next(err);
+        if (!coupon) return res.status(404).send('No coupon with that ID');
+        return res.sendStatus(200);
+    });
 };
