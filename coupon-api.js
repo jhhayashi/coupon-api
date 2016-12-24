83688471a372896d3a1cfd212f4c72ad2e69566e
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-24 12:09:07 -0800

    Add npm start command, only dev in non prod mode

--- coupon-api.js
+++ coupon-api.js
@@ -24,8 +24,10 @@ var router = express.Router();
 // run init script
 if (app.get('env') === 'production') require('./init/init');
 
+// log if in dev mode
+else app.use(logger('dev'));
+
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
