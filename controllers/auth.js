d3c8fb9bc17ab0919cd865087e9bf9c2b5b989f0
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-24 17:49:38 -0800

    Make error messages more friendly in login

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,9 @@ const config = require('../models/config');
 
 exports.loginUser = (req, res, next) => {
     if (typeof req.body.email !== 'string')
-        return res.status(400).send('No email');
+        return res.status(400).send('Missing email');
     if (typeof req.body.password !== 'string')
-        return res.status(400).send('No password');
+        return res.status(400).send('Missing password');
 
     User.findOne({email: req.body.email}, (err, user) => {
         if (err) return next(err);
@@ -14,6 +14,7 @@ exports.loginUser = (req, res, next) => {
         if (!user.isAdmin && !user.isSuperAdmin)
             return res.status(403).send('No admin wih that email');
         user.comparePassword(req.body.password, (err, isMatch) => {
+            if (err) return next(err);
             if (!isMatch)
                 return res.status(401).send('Incorrect password');
 
